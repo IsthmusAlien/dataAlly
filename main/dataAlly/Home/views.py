504c8ec3c6de5869b65d487bcf74f1b5110e9720
Dracgamer5643 <supermee5643@gmail.com>
2024-04-16 23:45:25 +0530

    Added Dataset sorting top 10 in Home Page

--- main/dataAlly/Home/views.py
+++ main/dataAlly/Home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from Database.models import Database
 
 def Home(request):
-    databases = Database.objects.all()
+    databases = Database.objects.all().order_by('-db_rating')[:10]
     return render(request, 'home.html', {'databases': databases})
