3ddd0fab421877e5d096363826d25d7510aa836e
Dracgamer5643 <supermee5643@gmail.com>
2024-04-17 12:02:53 +0530

    Done with the Database for Models and Database pages

--- main/dataAlly/Home/views.py
+++ main/dataAlly/Home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from Database.models import Database
+from Models.models import Models
 
 def Home(request):
     databases = Database.objects.all().order_by('-db_rating')[:10]
-    return render(request, 'home.html', {'databases': databases})
+    models = Models.objects.all().order_by('-md_rating')[:10]
+    return render(request, 'home.html', {'databases': databases, 'models': models})
--- main/dataAlly/Models/models.py
+++ main/dataAlly/Models/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Models(models.Model):
+    md_name = models.CharField(max_length=100, default="")
+    md_info = models.CharField(max_length=250, default="")
+    md_image = models.ImageField(upload_to='models/', default="")
+    md_file = models.FileField(upload_to='models_files/', default="")
+    md_owner_name = models.CharField(max_length=50, default="")
+    md_rating = models.FloatField(validators=[MaxValueValidator(5)], default=0)
+    md_upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.md_name
